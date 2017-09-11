6c0dc81d749a3686937d0699b79457d24e2a1924
skra8710 <kawakami.16@buckeyemail.osu.edu>
2017-09-11 13:23:24 -0400

    actually prints user input as part of puts now.

--- sample.rb
+++ sample.rb
@@ -1,13 +1,8 @@
 # quick hello world prog by Sakura
 puts 'Hello World!'
 
-<<PARAGRAPH
-AYYYYYYYYYYYYYYYY
-HERE'S A COOL ONE
-PARAGRAPH
-
 # ask for user input
 puts 'Tell me your favorite color: '
 color = gets.chomp
-puts 'Neato, well I like beige.'
-puts PARAGRAPH
+puts "Neato, #{color} is a nice color.'
+puts 'But I like beige."
